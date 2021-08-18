50e19fc4f46b65095dde65e6784501d63db7a93a
fikuri <myblogger2992@gmail.com>
2021-08-18 14:27:24 +0700

    add new table

---
+++ database/migrations/1629270731879_offer_schema.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class OfferSchema extends Schema {
+  up () {
+    this.create('offers', (table) => {
+      table.increments()
+      table.integer('user_id').unsigned().references('id').inTable('users');
+      table.integer('user_id').unsigned().references('id').inTable('users');
+      table.string('name', 255).nullable();
+      table.string('slug', 255).nullable();
+      table.text('description').nullable();
+      table.string('type', 100).nullable();
+      table.text('content').nullable();
+      table.text('content').nullable();
+      table.string('status', 100).nullable();
+      table.boolean('is_published').defaultTo(false);
+      table.integer('price').nullable();
+      table.integer('created_by').unsigned().nullable();
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('offers')
+  }
+}
+
+module.exports = OfferSchema
