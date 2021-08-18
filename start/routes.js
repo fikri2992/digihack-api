d71413f2c96f4904f48ed63b1d7198a89c04871e
Dvostrana <dvostrana@users.noreply.github.com>
2021-08-18 15:31:47 +0700

    add offer service, controller and route

--- start/routes.js
+++ start/routes.js
@@ -27,4 +27,5 @@ Route.group(() => {
 
 
 Route.resource('interactions', 'InteractionController').apiOnly().middleware(['auth']);
+Route.resource('offers', 'OfferController').apiOnly().middleware(['auth']);
 // Route.post('logout', 'AuthController.logout');
